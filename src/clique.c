36cb0a9e223521bfe61903fd9366c7dc14a2f0c0
Theodosis Paidakis <theodosis_paid@yahoo.gr>
2020-11-04 20:43:04 +0200

    Implemented all the frees we need

--- src/clique.c
+++ src/clique.c
@@ -12,3 +12,11 @@ CliqueNode *initCliqueNode()
 void updateCliques(char *leftSpecId, char *rightSpecId)
 {
 }
+
+void freeCliqueNode(CliqueNode *cliqueNode)
+{
+    if (cliqueNode == NULL)
+        return;
+    freeSpecInfo(cliqueNode->specInfo);
+    free(cliqueNode);
+}
