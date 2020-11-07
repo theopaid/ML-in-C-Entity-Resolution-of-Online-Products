372463cb37cc23ecd97c28237a8240dee084cf2f
Theodosis Paidakis <theodosis_paid@yahoo.gr>
2020-11-07 15:39:16 +0200

    Updated main to read user input (#23)

--- src/main.c
+++ src/main.c
@@ -5,39 +5,29 @@ double timeSpent;
 int main(int argc, char **argv)
 {
   clock_t begin = clock();
-  //printf("%s", hello());
-  //read_from_file(DATASETX);
-  //printf("%d\n", count_datafiles("./Datasets/2013_camera_specs/"));
-  //read_from_dir("./Datasets/2013_camera_specs/");
 
-  //HashBucket **hashTable = initHashTable(count_datafiles(DATASET_PATH));
+  if (!validArgs(argc, argv))
+  {
+    printf("The arguments given are invalid. Try again.\n");
+    return -1;
+  }
 
-  HashTable *hashTable = initHashTable(count_datafiles(DATASET_PATH));
+  char *datasetX, *datasetW;
+  getArgs(&datasetX, &datasetW, argv);
+  puts(datasetW);
+  puts(datasetX);
 
-  read_from_dir(DATASET_PATH, hashTable);
+  HashTable *hashTable = initHashTable(count_datafiles(datasetX));
+
+  read_from_dir(datasetX, hashTable);
+  readDictionary(datasetW);
 
   freeHashTable(hashTable);
 
-  /*SpecInfo *spec = initSpecInfo("hello", "world");
-  printf("%s\n", spec->specId);
-  printf("%s\n", spec->pageTitle);*/
   clock_t end = clock();
   timeSpent = (double)(end - begin) / CLOCKS_PER_SEC;
   printInsights();
   return 0;
-
-  // printf("%s", hello());
-  // read_from_file(DATASETX);
-  // if (!validArgs(argc, argv))
-  // {
-  //   printf("The arguments given are invalid. Try again.\n");
-  //   return -1;
-  // }
-
-  // char *datasetX, *datasetW;
-  // getArgs(&datasetX, &datasetW, argv);
-
-  // read_from_file(datasetX);
 }
 
 void printExecTime()
