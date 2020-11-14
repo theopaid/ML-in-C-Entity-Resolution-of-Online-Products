322eb0ab54236e134cc3edb00fd05da6651ce995
theopaid <theodosis_paid@yahoo.gr>
2020-11-14 12:49:32 +0200

    Cleaned some unnecessary code

--- test/test_initial.c
+++ test/test_initial.c
@@ -1,8 +1,6 @@
 #include <stddef.h>
 #include "Unity/src/unity.h"
 
-#include "../hdr/initial.h"
-
 void test_hashing();
 void test_clique();
 
@@ -14,27 +12,12 @@ void tearDown(void)
 {
 }
 
-void test_hello(void)
-{
-   TEST_ASSERT_EQUAL_STRING("Hello, world!\n", hello());
-}
-
 int main(void)
 {
-
-   /*SpecInfo *test_spec = initSpecInfo("site", "test_1.json", "test_2");
-   TEST_ASSERT_NOT_NULL(test_spec);
-   TEST_ASSERT_EQUAL_STRING("site//test_1", test_spec->specId);
-   TEST_ASSERT_EQUAL_STRING("test_2", test_spec->pageTitle);
-   //unitSpecInfo(test_spec);
-   freeSpecInfo(test_spec);
-}*/
-
    test_hashing();
    test_clique();
    return 0;
 }
-
 
 void printExecTime()
 {
