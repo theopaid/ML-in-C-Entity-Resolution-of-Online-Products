5707816b1a26b09ecdde2e5449d358f0f9f8f10a
Theodosis Paidakis <theodosis_paid@yahoo.gr>
2020-11-15 20:29:31 +0200

    small fix in test_spec

--- test/test_reading.c
+++ test/test_reading.c
@@ -5,8 +5,8 @@
 
 void test_count_datafiles(void) {
   TEST_ASSERT_EQUAL(2, count_datafiles("./test_files/"));
-  TEST_ASSERT_EQUAL(9, count_datafiles("./src/"));
-  TEST_ASSERT_EQUAL(10, count_datafiles("./hdr/"));
+  TEST_ASSERT_EQUAL(8, count_datafiles("./src/"));
+  TEST_ASSERT_EQUAL(9, count_datafiles("./hdr/"));
 }
 
 void test_reading()
