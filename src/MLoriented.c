4ff401df1d709dd7e9839f704917fe4d1ee1b85a
Theodosis Paidakis <theodosis_paid@yahoo.gr>
2020-12-14 21:26:55 +0200

    Added IDF implementation

--- src/MLoriented.c
+++ src/MLoriented.c
@@ -1,8 +1,10 @@
 #include "../hdr/includes.h"
 int specsInitiated = 0;
+double differentSpecs = 0.0;
 
 Vector *lastMLVector;
 Vector *stopWords;
+Vector *idfVector;
 
 MLInfo *initMLinfo(char *string)
 {
@@ -36,9 +38,9 @@ void initMLinChain(SpecNode *head)
     SpecNode *specPtr = head;
     while (specPtr != NULL)
     {
-        printf("SPEC: %s\n", specPtr->cliquePtr->specInfo->specId);
+        //printf("SPEC: %s\n", specPtr->cliquePtr->specInfo->specId);
         specsInitiated++;
-        printf("spec num: %d\n", specsInitiated);
+        //printf("spec num: %d\n", specsInitiated);
         initMLforSpec(specPtr);
         specPtr = specPtr->nextSpec;
     }
@@ -192,5 +194,169 @@ void freeMLinfo(Vector *vectorMLinfo)
     for (int i = 0; i < count; i++)
     {
         free(((MLInfo *)vectorMLinfo->items[i])->word);
+    }
+}
+
+idfInfo *initIDFinfo(char *string)
+{
+    idfInfo *newIDFinfo = safe_malloc(sizeof(idfInfo));
+    newIDFinfo->word = createString(string);
+    newIDFinfo->idfValue = 0.0;
+    newIDFinfo->timesSeen = 0.0;
+    newIDFinfo->alreadySeenInSpecFlag = 0;
+
+    return newIDFinfo;
+}
+
+void createIDFvector(HashTable *hashTable, Vector *stopwords)
+{
+    if (hashTable == NULL)
+        return;
+    stopWords = stopWords;
+    idfVector = vectorInit();
+    differentSpecs = 0;
+    for (int i = 0; i < hashTable->size; i++)
+    {
+        if (hashTable->hashArray[i] == NULL) // bucket not allocated, has no Specs
+            continue;
+        initIDFinChain(hashTable->hashArray[i]->specList);
+    }
+    computeIDFvalues(idfVector);
+    printIDFvector(idfVector);
+    freeIDFinfo(idfVector);
+    freeVector(idfVector);
+}
+
+void initIDFinChain(SpecNode *head)
+{
+    SpecNode *specPtr = head;
+    while (specPtr != NULL)
+    {
+        //printf("SPEC: %s\n", specPtr->cliquePtr->specInfo->specId);
+        specsInitiated++;
+        //printf("spec num: %d\n", specsInitiated);
+        addSpecToIDF(specPtr);
+        specPtr = specPtr->nextSpec;
+    }
+}
+
+void addSpecToIDF(SpecNode *specNode)
+{
+    differentSpecs++;
+    resetIDFSeenFlags(idfVector);
+    SpecInfo *specInfo = specNode->cliquePtr->specInfo;
+    if (specInfo == NULL)
+        return;
+    processWordAddToIDF("page");
+    processWordAddToIDF("title");
+    addSentenceToIDF(specInfo->pageTitle);
+    traverseInfoListToIDF(specInfo->infoList);
+}
+
+void addWordToIDF(char *wordToBeInserted)
+{
+    int itemsCount = vectorItemsCount(idfVector);
+    for (int i = 0; i < itemsCount; i++)
+    {
+        char *currentWord = ((idfInfo *)vectorGet(idfVector, i))->word;
+        if (same_string(currentWord, wordToBeInserted))
+        {
+            if (((idfInfo *)vectorGet(idfVector, i))->alreadySeenInSpecFlag == 0)
+            {
+                ((idfInfo *)vectorGet(idfVector, i))->timesSeen++;
+                ((idfInfo *)vectorGet(idfVector, i))->alreadySeenInSpecFlag = 1;
+            }
+            return;
+        }
+    }
+    // word not found, time to insert it
+    idfInfo *newIDFinfo = initIDFinfo(wordToBeInserted);
+    newIDFinfo->timesSeen = 1.0;
+    vectorPushBack(idfVector, newIDFinfo);
+}
+
+void computeIDFvalues(Vector *idfVector)
+{
+    int itemsCount = vectorItemsCount(idfVector);
+    for (int i = 0; i < itemsCount; i++)
+    {
+        ((idfInfo *)vectorGet(idfVector, i))->idfValue = log(differentSpecs / ((idfInfo *)vectorGet(idfVector, i))->timesSeen);
+    }
+}
+
+void printIDFvector(Vector *idfVector)
+{
+    puts("====================");
+    int itemsCount = vectorItemsCount(idfVector);
+    for (int i = 0; i < itemsCount; i++)
+    {
+        printf("%s : %f || ", ((idfInfo *)vectorGet(idfVector, i))->word, ((idfInfo *)vectorGet(idfVector, i))->idfValue);
+    }
+    puts("====================");
+}
+
+void addSentenceToIDF(char *sentence)
+{
+    char delim[12] = {
+        ' ',
+        ',',
+        '.',
+        ':',
+        '\n',
+        '&',
+        '\\',
+        '|',
+        '/',
+        '-',
+        '(',
+        ')',
+    };
+    char *ptr = strtok(sentence, delim);
+    while (ptr != NULL)
+    {
+        processWordAddToIDF(ptr);
+        ptr = strtok(NULL, delim);
+    }
+}
+
+void processWordAddToIDF(char *word)
+{
+    char *wordCopy = createString(word);
+    for (int i = 0; i < strlen(wordCopy); i++)
+    {
+        wordCopy[i] = tolower(wordCopy[i]);
+    }
+    if (!isInStopwords(wordCopy))
+        addWordToIDF(wordCopy);
+    free(wordCopy);
+}
+
+void traverseInfoListToIDF(InfoList *infoList)
+{
+    InfoNode *tmpInfoNode = infoList->head;
+    while (tmpInfoNode != NULL)
+    {
+        addSentenceToIDF(tmpInfoNode->description);
+        addSentenceToIDF(tmpInfoNode->content);
+
+        tmpInfoNode = tmpInfoNode->next;
+    }
+}
+
+void freeIDFinfo(Vector *idfVector)
+{
+    int count = vectorItemsCount(idfVector);
+    for (int i = 0; i < count; i++)
+    {
+        free(((idfInfo *)vectorGet(idfVector, i))->word);
+    }
+}
+
+void resetIDFSeenFlags(Vector *idfVector)
+{
+    int itemsCount = vectorItemsCount(idfVector);
+    for (int i = 0; i < itemsCount; i++)
+    {
+        ((idfInfo *)vectorGet(idfVector, i))->alreadySeenInSpecFlag = 0;
     }
 }
