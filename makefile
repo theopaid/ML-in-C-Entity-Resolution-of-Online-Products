769aa08a2f3d73d2cfdc581de3cbf0dbd61e9dc0
grekoskall <grekoskall@gmail.com>
2021-01-24 23:05:09 +0200

    Patched.

--- makefile
+++ makefile
@@ -61,7 +61,7 @@ b: build
 
 .PHONY: clean
 clean:
-	rm -rf *.o *.out *.out.dSYM tmp*
+	rm -rf *.o *.out *.out.dSYM tmp* ./output/*.txt
 .PHONY: ccd
 c: clean
 
--- src/multithreading.c
+++ src/multithreading.c
@@ -3,6 +3,7 @@
 double *dj;
 pthread_mutex_t dj_access;
 double *testAccuracy;
+pthread_mutex_t acc_access;
 
 static void cleanup_handler(void *arg)
 {
@@ -198,8 +199,10 @@ void calculate_accuracy(void *param)
     }
 
     int position = ((CalculateAccuracy *)param)->i;
+    pthread_mutex_lock(&acc_access);
     if ( testAccuracy[position] > 0.0) testAccuracy[position] = (testAccuracy[position] + (hits) / ((double)hits+no_hits))/2.0;
     else testAccuracy[position] = (hits) / ((double)hits + no_hits);
+    pthread_mutex_unlock(&acc_access);
 
 }
 
@@ -207,6 +210,7 @@ double timeSpentTesting;
 
 double model_testing_testing(HashTable *hash_table, Vector *full_T_pairs, double *b, int _threads, float _threshold, int _batch_size)
 {
+    pthread_mutex_init(&acc_access, NULL);
     puts("==> Initiating model TESTING ...");
     //clock_t testing_start = clock();
     double accuracy = 0.0;
@@ -339,6 +343,7 @@ double model_testing(HashTable *hash_table, Vector *full_V_pairs, double *b)
     testAccuracy = NULL;
     //timeSpentTesting = (double)(validation_end - validation_start) / CLOCKS_PER_SEC;
     //printf("==> [+++] Validation Time [ %f ]\n", timeSpentTesting);
+    pthread_mutex_destroy(&acc_access);
     return accuracy;
 }
 
@@ -353,8 +358,8 @@ double *train_weights(HashTable *hash_table, HashTable_w *W1, Vector *full_W_pai
     float threshold = THRESHOLD_VALUE;
     dj = (double *)calloc(TF_IDF_SIZE * 2, sizeof(double));
     pthread_mutex_init(&dj_access, NULL);
-
-    while (threshold < 0.5)
+    int finish = 0;
+    while (threshold < 0.5 && finish < 3)
     {
         puts("==> Training model weights ...");
         printf("==> Threads that are being used: %d ...\n", THREADS_NUM);
@@ -399,6 +404,7 @@ double *train_weights(HashTable *hash_table, HashTable_w *W1, Vector *full_W_pai
         puts("==> Resolving transitivity issues between pairs ...");
         resolve_transitivity(hash_table, new_pair_not_in_W, W1); //
         threshold += THRESHOLD_STEP * THRESHOLD_SLOPE;
+        finish++;
     }
     free(dj);
     pthread_mutex_destroy(&dj_access);
@@ -430,14 +436,14 @@ void calculate_dj(void *param)
             {
                 if (((Observation *)((CalculateDJ *)param)->pairs->items[i])->left_tf_idf->itemsInserted <= j)
                     break;
-                if (((Observation *)((CalculateDJ *)param)->pairs->items[i])->left_tf_idf->items[i] == NULL)
+                if (((Observation *)((CalculateDJ *)param)->pairs->items[i])->left_tf_idf->items[j] == NULL)
                     continue;
             }
             else
             {
                 if (((Observation *)((CalculateDJ *)param)->pairs->items[i])->right_tf_idf->itemsInserted <= j - TF_IDF_SIZE)
                     break;
-                if (((Observation *)((CalculateDJ *)param)->pairs->items[i])->right_tf_idf->items[i] == NULL)
+                if (((Observation *)((CalculateDJ *)param)->pairs->items[i])->right_tf_idf->items[j] == NULL)
                     continue;
             }
             double px = p_logistic_function_full(((CalculateDJ *)param)->pairs->items[i], ((CalculateDJ *)param)->b);
